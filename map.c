0df21c818d7ce9e7fe99720aa230839b1bacdf65
JulienPR86 <julien.pr86@gmail.com>
2024-11-14 18:48:36 +0100

    change sign of comparaison

--- map.c
+++ map.c
@@ -89,7 +89,7 @@ void map_display(SDL_Renderer *renderer, Map *map)
 
 int map_get_square_size(int screen_width, int screen_height, int map_width, int map_height)
 {
-    if (screen_width/map_width > screen_height/map_height)
+    if (screen_width/map_width < screen_height/map_height)
     {
         return screen_width/map_width;
     }
