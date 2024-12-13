c749b661b06b55746279f9d312c18807a2645876
JulienPR86 <julien.pr86@gmail.com>
2024-12-13 15:31:08 +0100

    feat : modified struct button for images instead of colors

--- scripts/button.c
+++ scripts/button.c
@@ -8,33 +8,34 @@ int button_update(Button *button)
     {
         if (mouse_button_pressed == 1)
         {
-            button->color = button->color_on_click;
+            button->img = button->img_pressed;
             button->clicked = 1;
         }
         else if (mouse_button_pressed == 0 && button->clicked)
         {
             button->clicked = 0;
-            button->color = button->color_out;
+            button->img = button->img_unpressed;
             out = button->command();
         }
         else
         {
-            button->color = button->color_out;
+            button->img = button->img_unpressed;
         }
     }
     else
     {
         if (button->clicked){button->clicked = 0;}
-        button->color = button->color_out;
+        button->img = button->img_unpressed;
     }
     return out;
 }
 
 void button_render(Button *button)
 {
-    SDL_SetRenderDrawColor(renderer, button->color.r, button->color.g, button->color.b, button->color.a);
     SDL_Rect button_rect = {button->x, button->y, button->width, button->height};
-    SDL_RenderFillRect(renderer, &button_rect);
+    SDL_Texture *img = SDL_CreateTextureFromSurface(renderer, button->img);
+    SDL_RenderCopy(renderer, img, NULL, &button_rect);
+
 }
 
 int button_collision(Button *button, int x, int y)
