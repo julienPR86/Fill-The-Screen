4b6e45d9ffdfdcf81063aea1e198ab29381b6c2a
Julien <julien.pr86@gmail.com>
2025-02-12 14:58:23 +0100

    + added SDL function verificatin + update verification in label render

--- scripts/label.c
+++ scripts/label.c
@@ -2,39 +2,69 @@
 
 Label *label_init(Label *label)
 {
-    label_destroy(label);
-    if (NULL == label->text)
+    if (NULL == label || NULL == label->text)
         return NULL;
-    TTF_SizeText(label->font, label->text, &label->w, &label->h);
+
+    if (0 != TTF_SizeText(label->font, label->text, &label->w, &label->h))
+    {
+        fprintf(stderr, "Size text error : %s\n", TTF_GetError());
+    }
+    
     label->surface = TTF_RenderText_Blended(label->font, label->text, label->text_color);
+    if (NULL == label->surface)
+    {
+        fprintf(stderr, "Surface allocation error : %s\n", TTF_GetError());
+    }
+
     label->texture = SDL_CreateTextureFromSurface(renderer, label->surface);
+    if (NULL == label->texture)
+    {
+        fprintf(stderr, "Texture allocation error : %s\n", SDL_GetError());
+    }
+
     if (label->bg.a < 255 || label->text_color.a < 255)
-        SDL_SetTextureBlendMode(label->texture, SDL_BLENDMODE_BLEND);
+    {
+        if (0 != SDL_SetTextureBlendMode(label->texture, SDL_BLENDMODE_BLEND))
+        {
+            fprintf(stderr, "Cannot set texture blend mode : %s\n", SDL_GetError());
+        }
+    }
     return label;
 }
 
 void label_render(Label *label)
 {
-    if (NULL == label->text)
+    if (NULL == label || NULL == label->texture)
         return;
+    
+    if (label->update)
+    {
+        label_free(label);
+        label_init(label);
+        label->update = false;
+    }
     SDL_Rect label_rect = {label->x, label->y, label->w*label->scale, label->h*label->scale};
-    SDL_RenderCopy(renderer, label->texture, NULL, &label_rect);
+    if (0 != SDL_RenderCopy(renderer, label->texture, NULL, &label_rect))
+    {
+        fprintf(stderr, "Render copy error : %s\n", SDL_GetError());
+    }
     return;
 }
 
-Label *label_destroy(Label *label)
+void label_free(Label *label)
 {
     if (NULL == label)
-    {
-        return NULL;
-    }
-    if (NULL == label->texture || NULL == label->surface)
-    {
         return;
+    
+    if (NULL != label->texture)
+    {
+        SDL_DestroyTexture(label->texture);
+        label->texture = NULL;
     }
-    SDL_DestroyTexture(label->texture);
-    label->texture = NULL;
-    SDL_FreeSurface(label->surface);
-    label->surface = NULL;
-    return label;
+    if (NULL != label->surface)
+    {
+        SDL_FreeSurface(label->surface);
+        label->surface = NULL;
+    }
+    return;
 }
