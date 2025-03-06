bc71da0f542ec4dd36317b67db9d3b3d45b13a5e
Julien <julien.pr86@gmail.com>
2025-03-06 12:33:52 +0100

    + added panel set button and label at index functions declaration

--- scripts/panel.c
+++ scripts/panel.c
@@ -79,6 +79,23 @@ void panel_render(Panel *panel)
     return;
 }
 
+int panel_set_button_at_index(Panel *panel, Button *button, int index)
+{
+    if (NULL == panel || NULL == panel->buttons || index < 0 || index >= panel->button_count)
+        return RETURN_FAILURE;
+
+    panel->buttons[index] = button;
+    return RETURN_SUCCESS;
+}
+
+int panel_set_label_at_index(Panel *panel, Label *label, int index)
+{
+    if (NULL == panel || NULL == panel->labels || index < 0 || index >= panel->label_count)
+        return RETURN_FAILURE;
+
+    panel->labels[index] = label;
+    return RETURN_SUCCESS;
+}
 
 void panel_free(Panel *panel)
 {
