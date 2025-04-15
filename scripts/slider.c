63f91689adcd562f95faa7033eda624d7b76eea3
Julien <julien.pr86@gmail.com>
2025-04-15 13:24:42 +0100

    # updated slider update to make it easier to control

--- scripts/slider.c
+++ scripts/slider.c
@@ -56,43 +56,58 @@ int slider_update(Slider *slider)
     switch (out)
     {
         case RETURN_SLIDER_UPDATE:
-            if (slider->cursor->x + slider->cursor->size / 2 < slider->x)
-                slider->cursor->x = slider->x - slider->cursor->size / 2;
-            else if (slider->cursor->x + slider->cursor->size / 2 > slider->x + slider->w)
-                slider->cursor->x = slider->x + slider->w - slider->cursor->size / 2;
-
-            slider->label->x = slider->cursor->x + CENTERED(slider->cursor->size, slider->label->w * slider->label->scale);
-            slider->cursor->y = slider->y + CENTERED(slider->h, slider->cursor->size);
-
-            if (NULL == slider->value)
-                break;
-            
-            if (NULL != slider->label->text)
-            {
-                free(slider->label->text);
-                slider->label->text = NULL;
-            }
-            
-            slider->label->text = (char *)malloc((get_number_digits(slider->max)+1) * sizeof(char));
-            if (NULL == slider->label->text)
-            {
-                fprintf(stderr, "slider->label->text : Memory allocation error");
-            }
-            snprintf(slider->label->text, (get_number_digits(slider->max)+1), "%d", *slider->value);
-            slider->label->update = true;
-
-            *slider->value = slider->min + (slider->max - slider->min) * (slider->cursor->x + slider->cursor->size / 2 - slider->x) / slider->w / slider->step * slider->step;
-            
-            if (*slider->value < slider->min)
-                *slider->value = slider->min;
-
-            if (*slider->value > slider->max)
-                *slider->value = slider->max;
             break;
-        
+            
         default:
             break;
     }
+    
+    if (RETURN_NONE == out)
+    {    
+        if (slider_collision(slider, mouse_x, mouse_y) || slider->cursor->state == CLICKED)
+        {
+            if (mouse_button_pressed == 1)
+            {
+                slider->cursor->state = CLICKED;
+                slider->cursor->x += mouse_delta_x;
+                out = RETURN_SLIDER_UPDATE;
+            }
+        }
+    }
+    
+    if (RETURN_NONE != out && NULL != slider->label && NULL != slider->value)
+    {
+        if (slider->cursor->x + slider->cursor->size / 2 < slider->x)
+            slider->cursor->x = slider->x - slider->cursor->size / 2;
+        else if (slider->cursor->x + slider->cursor->size / 2 > slider->x + slider->w)
+            slider->cursor->x = slider->x + slider->w - slider->cursor->size / 2;
+
+        slider->label->x = slider->cursor->x + CENTERED(slider->cursor->size, slider->label->w * slider->label->scale);
+        // slider->cursor->y = slider->y + CENTERED(slider->h, slider->cursor->size);
+
+        if (NULL != slider->label->text)
+        {
+            free(slider->label->text);
+            slider->label->text = NULL;
+        }
+            
+        slider->label->text = (char *)malloc((get_number_digits(slider->max)+1) * sizeof(char));
+        if (NULL == slider->label->text)
+        {
+            fprintf(stderr, "slider->label->text : Memory allocation error");
+        }
+        snprintf(slider->label->text, (get_number_digits(slider->max)+1), "%d", *slider->value);
+        slider->label->update = true;
+
+        *slider->value = slider->min + (slider->max - slider->min) * (slider->cursor->x + slider->cursor->size / 2 - slider->x) / slider->w / slider->step * slider->step;
+            
+        if (*slider->value < slider->min)
+            *slider->value = slider->min;
+
+        if (*slider->value > slider->max)
+            *slider->value = slider->max;
+    }
+    
     label_update(slider->label);
     return out;
 }
