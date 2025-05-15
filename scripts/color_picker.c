ee56b79a4815eb49d6dfc840b283a5a8881ac344
julienPR86 <julien.pr86@gmail.com>
2025-05-15 17:44:48 +0200

    + created Color picker file with all color picker func definition base

---
+++ scripts/color_picker.c
@@ -0,0 +1,36 @@
+#include "../main.h"
+
+ColorPicker *picker_init(ColorPicker *picker)
+{
+    return picker;
+}
+
+int picker_update(ColorPicker *picker, float scale)
+{
+    return 0;
+}
+
+void picker_render(ColorPicker *picker, float scale)
+{
+    return;
+}
+
+void picker_free(ColorPicker *picker)
+{
+    return;
+}
+
+void picker_list_free(ColorPicker *pickers[], int size)
+{
+    return;
+}
+
+int picker_height(ColorPicker *picker, float scale)
+{
+    return;
+}
+
+int picker_width(ColorPicker *picker, float scale)
+{
+    return;
+}
