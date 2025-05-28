b4cbc21ff0279904969d9763ac42ef85f1075eb7
Julien <julien.pr86@gmail.com>
2025-05-28 11:13:27 +0200

    # updated get width func

--- scripts/slider.c
+++ scripts/slider.c
@@ -251,5 +251,5 @@ int slider_width(Slider *slider, float scale)
     if (NULL == slider)
         return 0;
 
-    return (slider->rect.width + slider->style->outline * 2) * scale;
+    return (slider->rect.width + slider->style->outline * 2 + slider->label->rect.width + 5) * scale;
 }
