db320a69c6d3968496b699215de1d16ac1f235e5
Julien <julien.pr86@gmail.com>
2025-06-02 18:58:45 +0200

    updated render_outline func

--- scripts/rect.c
+++ scripts/rect.c
@@ -16,11 +16,18 @@ void rect_render(Rect *rect, float scale)
 
 void render_outline(UI_Element *element, float scale)
 {
-    SDL_FRect outline_rect = {element->x - element->outline.size * scale, element->y - element->outline.size * scale, element->width * scale + element->outline.size * 2 * scale, element->height * scale + element->outline.size * 2 * scale};
-    SDL_FRect inline_rect = {element->x, element->y, element->width, element->height};
+    if (element->outline.size > 0)
+    {
+        SDL_FRect outline_rect = {element->x - element->outline.size * scale, element->y - element->outline.size * scale, element->width * scale + element->outline.size * 2 * scale, element->height * scale + element->outline.size * 2 * scale};
     
-    SDL_SetRenderDrawColor(renderer, element->outline.color.r, element->outline.color.g, element->outline.color.b, element->outline.color.a);
-    SDL_RenderFillRect(renderer, &outline_rect);
+        SDL_SetRenderDrawColor(renderer, element->outline.color.r, element->outline.color.g, element->outline.color.b, element->outline.color.a);
+        SDL_RenderFillRect(renderer, &outline_rect);
+    }
+
+    if (element->inline_.size <= 0)
+        return;
+
+    SDL_FRect inline_rect = {element->x, element->y, element->width, element->height};
 
     SDL_SetRenderDrawColor(renderer, element->inline_.color.r, element->inline_.color.g, element->inline_.color.b, element->inline_.color.a);
     SDL_RenderFillRect(renderer, &inline_rect);
