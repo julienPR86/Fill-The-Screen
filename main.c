b94050cab757055fd25b9790e84e855d8fd0c732
Julien <julien.pr86@gmail.com>
2025-06-06 13:54:07 +0200

    # modified some elements positions and anchors

--- main.c
+++ main.c
@@ -7,20 +7,20 @@ int main()
         fprintf(stderr, "Could not initialised the game\n");
         return RETURN_FAILURE;
     }
-    Label title_label = {EMPTY_UI_ELEMENT, "Fill The Screen", 100, {255, 0, 0, 255}, NULL, NULL, false, true};
+    Label title_label = {{WIDTH/2, 50, 0, 0, NO_OUTLINE, NO_INLINE, CENTER}, "Fill The Screen", 100, {255, 0, 0, 255}, NULL, NULL, false, true};
     label_init(&title_label, SCALE);
 
-    Label play_button_label = {EMPTY_UI_ELEMENT, "PLAY", 30, {0, 0, 0, 255}, NULL, NULL, false, true};
+    Label play_button_label = {{0, 0, 0, 0, NO_OUTLINE, NO_INLINE, CENTER}, "PLAY", 30, {0, 0, 0, 255}, NULL, NULL, false, true};
     Label options_button_label = {EMPTY_UI_ELEMENT, "Options", 30, {0, 0, 0, 255}, NULL, NULL, false, true};
     Label exit_button_label = {EMPTY_UI_ELEMENT, "Quit", 30, {0, 0, 0, 255}, NULL, NULL, false, true};
 
-    Button play_button = {{20, 200, 150, 50, outlines[2], inlines[1], TOP_LEFT}, NORMAL, &play_button_label, &button_style, &mode_choice, true};
+    Button play_button = {{WIDTH/2, HEIGHT/2, 150, 50, outlines[2], inlines[1], CENTER}, NORMAL, &play_button_label, &button_style, &mode_choice, true};
     button_init(&play_button, SCALE);
 
-    Button options_button = {{20, 300, 150, 50, outlines[2], inlines[1], TOP_LEFT}, NORMAL, &options_button_label, &button_style, &options, true};
+    Button options_button = {{WIDTH/2, HEIGHT/2+100, 150, 50, outlines[2], inlines[1], CENTER}, NORMAL, &options_button_label, &button_style, &options, true};
     button_init(&options_button, SCALE);
 
-    Button exit_button = {{20, 400, 150, 50, outlines[2], inlines[1], TOP_LEFT}, NORMAL, &exit_button_label, &button_style, &exit_game, true};
+    Button exit_button = {{WIDTH/2, HEIGHT/2+200, 150, 50, outlines[2], inlines[1], CENTER}, NORMAL, &exit_button_label, &button_style, &exit_game, true};
     button_init(&exit_button, SCALE);
     
     Label *labels[] = {&title_label, &FPS_label};
