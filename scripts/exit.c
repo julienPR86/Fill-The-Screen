139016bddd13667a3d268e3541d37276a334b908
JulienPR86 <julien.pr86@gmail.com>
2025-07-19 12:01:15 +0200

    moved exit_full_game to exit.c

--- scripts/exit.c
+++ scripts/exit.c
@@ -1,1 +1,25 @@
 #include "../main.h"
+
+void exit_full_game()
+{
+    label_free(&FPS_label);
+    free(FPS_text);
+    FPS_text = NULL;
+
+    free(inlines);
+    free(outlines);
+    for (int i = 0; i < max_font_size; i++)
+    {
+        TTF_CloseFont(roboto_regular_fonts[i]);
+    }
+    free(roboto_regular_fonts);
+
+    SDL_DestroyRenderer(renderer);
+    renderer = NULL;
+    SDL_DestroyWindow(window);
+    window = NULL;
+
+    TTF_Quit();
+    SDL_Quit();
+    return;
+}
