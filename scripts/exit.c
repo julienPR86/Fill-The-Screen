4fa43ea90e6977788e4c0343cf5ca727ed25c256
JulienPR86 <julien.pr86@gmail.com>
2025-07-19 12:06:15 +0200

    + adde comments

--- scripts/exit.c
+++ scripts/exit.c
@@ -2,23 +2,30 @@
 
 void exit_full_game()
 {
+    //free the FPS label
     label_free(&FPS_label);
     free(FPS_text);
     FPS_text = NULL;
 
+    //free the outlines and inlines list
     free(inlines);
     free(outlines);
+
+    //Close all the fonts
     for (int i = 0; i < max_font_size; i++)
     {
         TTF_CloseFont(roboto_regular_fonts[i]);
     }
+    //free the fonts list
     free(roboto_regular_fonts);
 
+    //destroy the renderer and the window
     SDL_DestroyRenderer(renderer);
     renderer = NULL;
     SDL_DestroyWindow(window);
     window = NULL;
 
+    //quit TTF and SDL
     TTF_Quit();
     SDL_Quit();
     return;
