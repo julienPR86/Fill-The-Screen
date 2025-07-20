d4114ed801c0d7ffc39dcde353e4102e82db4da3
JulienPR86 <julien.pr86@gmail.com>
2025-07-20 11:46:21 +0200

    updated UI_Element scale uses

--- scripts/UI/UI_Element.c
+++ scripts/UI/UI_Element.c
@@ -18,11 +18,11 @@ void UI_Element_match_size(UI_Element *element, UI_Element *frame, float scale_x
     if (NULL == element || NULL == frame)
         return;
 
-    if (element->width * scale_x < frame->width)
+    if (element->width * element->scale * scale_x < frame->width)
     {
         element->width = frame->width;
     }
-    if (element->height * scale_y < frame->height)
+    if (element->height * element->scale * scale_y < frame->height)
     {
         element->height = frame->height;
     }
@@ -34,7 +34,7 @@ int UI_element_collision(UI_Element *element, int x, int y, float scale_x, float
     UI_Element new_element = *element;
     UI_Element_set_position(&new_element, element->x, element->y, scale_x, scale_y, scale_x, scale_y, element->anchor);
 
-    if (x >= new_element.x - new_element.outline.size * scale_x && x < new_element.x + (element->width + new_element.outline.size) * scale_x && y >= new_element.y - new_element.outline.size * scale_y && y < new_element.y + (new_element.height + new_element.outline.size) * scale_y)
+    if (x >= new_element.x - new_element.outline.size * scale_x && x < new_element.x + (element->width * element->scale + new_element.outline.size) * scale_x && y >= new_element.y - new_element.outline.size * scale_y && y < new_element.y + (new_element.height * element->scale + new_element.outline.size) * scale_y)
     {
         return true;
     }
@@ -49,39 +49,39 @@ void UI_Element_set_position(UI_Element *element, int x, int y, float x_position
     switch (anchor)
     {
         case CENTER:
-            element->x = x * x_position_factor - (element->width / 2 + element->outline.size) * scale_x;
+            element->x = x * x_position_factor - (element->width / 2 + element->outline.size) * scale_x * element->scale;
             element->y = y * y_position_factor - (element->height / 2 + element->outline.size) * scale_y;
             break;
 
         case TOP_CENTER:
-            element->x = x * x_position_factor - (element->width / 2 + element->outline.size) * scale_x;
+            element->x = x * x_position_factor - (element->width / 2 + element->outline.size) * scale_x * element->scale;
             break;
 
         case TOP_RIGHT:
-            element->x = x * x_position_factor - (element->width + element->outline.size) * scale_x;
+            element->x = x * x_position_factor - (element->width + element->outline.size) * scale_x * element->scale;
             break;
 
         case MID_LEFT:
-            element->y = y * y_position_factor - (element->height / 2 + element->outline.size) * scale_y;
+            element->y = y * y_position_factor - (element->height / 2 + element->outline.size) * scale_y * element->scale;
             break;
 
         case MID_RIGHT:
-            element->x = x * x_position_factor - (element->width + element->outline.size) * scale_x;
-            element->y = y * y_position_factor - (element->height / 2 + element->outline.size) * scale_y;
+            element->x = x * x_position_factor - (element->width + element->outline.size) * scale_x * element->scale;
+            element->y = y * y_position_factor - (element->height / 2 + element->outline.size) * scale_y * element->scale;
             break;
 
         case BOTTOM_LEFT:
-            element->y = y * y_position_factor - (element->height + element->outline.size) * scale_y;
+            element->y = y * y_position_factor - (element->height + element->outline.size) * scale_y * element->scale;
             break;
 
         case BOTTOM_CENTER:
-            element->x = x * x_position_factor - (element->width / 2 + element->outline.size) * scale_x;
-            element->y = y * y_position_factor - (element->height + element->outline.size) * scale_y;
+            element->x = x * x_position_factor - (element->width / 2 + element->outline.size) * scale_x * element->scale;
+            element->y = y * y_position_factor - (element->height + element->outline.size) * scale_y * element->scale;
             break;
 
         case BOTTOM_RIGHT:
-            element->x = x * x_position_factor - (element->width + element->outline.size) * scale_x;
-            element->y = y * y_position_factor - (element->height + element->outline.size) * scale_y;
+            element->x = x * x_position_factor - (element->width + element->outline.size) * scale_x * element->scale;
+            element->y = y * y_position_factor - (element->height + element->outline.size) * scale_y * element->scale;
             break;
 
         case NONE:
@@ -104,8 +104,8 @@ void UI_Element_render_outline(UI_Element *element, float scale_x, float scale_y
     {
         outline_rect.x = element->x - i;
         outline_rect.y = element->y - i;
-        outline_rect.w = element->width * scale_x + i * 2;
-        outline_rect.h = element->height * scale_y + i * 2;
+        outline_rect.w = element->width * element->scale * scale_x + i * 2;
+        outline_rect.h = element->height * element->scale * scale_y + i * 2;
         SDL_RenderRect(renderer, &outline_rect);
     }
     return;
@@ -123,8 +123,8 @@ void UI_Element_render_inline(UI_Element *element, float scale_x, float scale_y)
     {
         inline_rect.x = element->x + i;
         inline_rect.y = element->y + i;
-        inline_rect.w = element->width * scale_x - i * 2;
-        inline_rect.h = element->height * scale_y - i * 2;
+        inline_rect.w = element->width * element->scale * scale_x - i * 2;
+        inline_rect.h = element->height * element->scale * scale_y - i * 2;
         SDL_RenderRect(renderer, &inline_rect);
     }
     return;
@@ -132,10 +132,10 @@ void UI_Element_render_inline(UI_Element *element, float scale_x, float scale_y)
 
 int UI_Element_get_height(UI_Element *element, float scale_y)
 {
-    return (element->height + element->outline.size * 2) * scale_y;
+    return (element->height * element->scale + element->outline.size * 2) * scale_y;
 }
 
 int UI_Element_get_width(UI_Element *element, float scale_x)
 {
-    return (element->width + element->outline.size * 2) * scale_x;
+    return (element->width * element->scale + element->outline.size * 2) * scale_x;
 }
