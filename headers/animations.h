6d966b3df86f195c6d399bc14e4c2bd305108648
JulienPR86 <julien.pr86@gmail.com>
2025-07-22 15:10:07 +0200

    + created FloatAnimation structure

--- headers/animations.h
+++ headers/animations.h
@@ -1,6 +1,23 @@
 #ifndef _ANIMATIONS_H_
 #define _ANIMATION_H_
 
+//Modify a float value through a constant value change
+typedef struct FloatAnimation
+{
+    //the value to animate
+    float *value;
+    //a copy of the first value of value
+    float cache_value;
+    //the value to reach
+    float goal_value;
+    //the number of frame to do the animation
+    int frame_time;
+    //frame counter to do the animation
+    int frame_counter;
+    //active boolean to activate or des-activate the animation
+    int active;
+} FloatAnimation;
+
 
 
 #endif
