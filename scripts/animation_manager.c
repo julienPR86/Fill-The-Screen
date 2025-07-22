0628413e97333e412bb7aa623773fac0c7e6566b
JulienPR86 <julien.pr86@gmail.com>
2025-07-22 15:39:53 +0200

    + added animation_manager_update function

--- scripts/animation_manager.c
+++ scripts/animation_manager.c
@@ -1,2 +1,20 @@
 #include "../main.h"
 
+void animation_manager_update(AnimationManager *manager)
+{
+    if (NULL == manager)
+        return;
+    
+    int i;
+    if (NULL != manager->float_animations)
+    {
+        for (i = 0; i < manager->float_animation_count; i++)
+        {
+            if (NULL == manager->float_animations[i])
+                continue;
+                
+            float_animation_update(manager->float_animations[i]);
+        }
+    }
+    return;
+}
