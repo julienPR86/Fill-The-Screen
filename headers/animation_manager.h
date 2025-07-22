de12d355aac755e55947818a4501161a2570fdcc
JulienPR86 <julien.pr86@gmail.com>
2025-07-22 15:23:11 +0200

    + created Animation Manager structure

--- headers/animation_manager.h
+++ headers/animation_manager.h
@@ -1,6 +1,11 @@
 #ifndef _ANIMATION_MANAGER_H_
 #define _ANIMATION_MANAGER_H_
 
+#include "animations.h"
 
+typedef struct AnimationManager
+{
+    FloatAnimation **float_animations;
+} AnimationManager;
 
 #endif
