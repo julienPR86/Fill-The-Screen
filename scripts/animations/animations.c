ea29299268de334e5eb2af1cc30a4dff734fdc1c
JulienPR86 <julien.pr86@gmail.com>
2025-07-23 12:06:52 +0200

    created new folder for animations

---
+++ scripts/animations/animations.c
@@ -0,0 +1,51 @@
+#include "../../main.h"
+
+FloatAnimation *float_animation_create()
+{
+    FloatAnimation *animation;
+    animation = (FloatAnimation *)malloc(sizeof(FloatAnimation));
+    if (NULL == animation)
+    {
+        fprintf(stderr, "Memory allocation error : Failed to create float animation\n");
+        return NULL;
+    }
+    return animation;
+}
+
+int float_animation_set_fields(FloatAnimation *animation, float *value, float goal, int frame_time, int active)
+{
+    if (NULL == animation || NULL == animation->value)
+        return -1;
+    
+    animation->value = value;
+    animation->cache_value = *value;
+    animation->goal_value = goal;
+    animation->frame_time = frame_time;
+    animation->frame_counter = 0;
+    animation->active = active;
+    return 0;
+}
+
+int float_animation_update(FloatAnimation *animation)
+{
+    if (NULL == animation || NULL == animation->value || !animation->active || animation->frame_counter > animation->frame_time)
+        return -1;
+    
+    *animation->value = LERP(animation->cache_value, animation->goal_value, (animation->frame_counter/(float)animation->frame_time));
+    animation->frame_counter++;
+    if (animation->frame_counter > animation->frame_time)
+    {
+        animation->active = false;
+        animation->frame_counter = 0;
+    }
+    return 0;
+}
+
+int float_animation_destroy(FloatAnimation *animation)
+{
+    if (NULL == animation)
+        return -1;
+    
+    free(animation);
+    return 0;
+}
