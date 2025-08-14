bce233ecd2abc97e0a6c42a36adcce4f08631def
JulienPR86 <julien.pr86@gmail.com>
2025-08-14 14:37:07 +0200

    + created array struct

--- headers/UI/array.h
+++ headers/UI/array.h
@@ -3,4 +3,13 @@
 
 #include "../../main.h"
 
+typedef struct Array
+{
+    UI_Element **elements;
+    int element_count;
+    int max_per_row;
+    int max_per_column;
+    int padding;
+} Array;
+
 #endif
