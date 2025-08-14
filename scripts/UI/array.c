9b9d73a4129ccadf52a6d468d1b88056b3062c80
JulienPR86 <julien.pr86@gmail.com>
2025-08-14 14:47:15 +0200

    + added array_set_fields function coez

--- scripts/UI/array.c
+++ scripts/UI/array.c
@@ -5,6 +5,11 @@ int array_set_fields(Array *array, UI_Element **elements, int element_count, int
     if (NULL == array || NULL == elements || 0 == element_count)
         return -1;
 
+    array->elements = elements;
+    array->element_count = element_count;
+    array->max_row_number = max_row_number;
+    array->max_column_number = max_column_number;
+    array->padding = padding;
     return 0;
 }
 Array *array_init(Array *array)
