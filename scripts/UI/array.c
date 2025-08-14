40e260bec0f857eccc208d932644f67fafacaf2b
JulienPR86 <julien.pr86@gmail.com>
2025-08-14 14:45:24 +0200

    + added NULL statement check

--- scripts/UI/array.c
+++ scripts/UI/array.c
@@ -2,9 +2,15 @@
 
 int array_set_fields(Array *array, UI_Element **elements, int element_count, int max_row_number, int max_column_number, int padding)
 {
+    if (NULL == array || NULL == elements || 0 == element_count)
+        return -1;
+
     return 0;
 }
 Array *array_init(Array *array)
 {
+    if (NULL == array)
+        return NULL;
+        
     return array;
 }
