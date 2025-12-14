f581c8beb777871042793c791dd04fb2f9c3823e
julienPR86 <julien.pr86@gmail.com>
2025-12-14 21:33:01 +0100

    # updated rect header

--- headers/UI/rect.h
+++ headers/UI/rect.h
@@ -1,23 +1,23 @@
-#ifndef _RECT_H_
-#define _RECT_H_
+#ifndef RECT_H
+#define RECT_H
 
 #include "../color.h"
 #include "UI_Element.h"
 
-//Rect struture
+//Structure that represent a rectangle with an unique color
 typedef struct Rect
 {
-    //The element rect, contains the x, y, width, height, outline and inline values
+    //The rectangle rect, contains position and dimensions
     UI_Element rect;
-    //rect color
+    //The rectangle color
     Color *rect_color;
 } Rect;
 
-//Set the fields of the rect passed in
+//Set the fields of the rectangle with the values passed in
 void rect_set_fields(Rect *rect, Color *color);
-//render a rectangle
+//Renders the rectangle passed in
 void rect_render(Rect *element, float scale_x, float scale_y);
-
+//Updates and renders an array of rectangles
 int rect_list_update_and_render(Rect *rects[], int count);
 
 #endif
