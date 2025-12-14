0de4d8c440001b9c708b25745228ea663bda7d0d
julienPR86 <julien.pr86@gmail.com>
2025-12-14 21:52:04 +0100

    # updated toggle group header

--- headers/UI/toggle_group.h
+++ headers/UI/toggle_group.h
@@ -1,36 +1,36 @@
-#ifndef _TOGGLE_GROUP_H_
-#define _TOGGLE_GROUP_H_
+#ifndef TOGGLE_GROUP_H
+#define TOGGLE_GROUP_H
 
 #include "toggle.h"
 
-//toggle group structure
+//Structure that represent a group of toggle, only one can be pressed at a time
 typedef struct ToggleGroup
 {
-    //toggle group
+    //The toggles list
     Toggle **toggles;
-    //toggle count
+    //The number of toggles in the toggles list
     int count;
-    //the selected index
+    //The index of the actual selected toggle
     int selected;
-    //is active parameter
-    int active;
+    //Tells if the toggle group is active or not
+    t_uint8 active;
 } ToggleGroup;
 
-//Set the fields of toggle group based on the values passed in
-ToggleGroup *group_set_fields(ToggleGroup *group, Toggle *toggles[], int count, int selected, int active);
-//toggle group init, needs to be called before all toggle group functions
+//Set the fields of the toggle group based on the values passed in
+ToggleGroup *group_set_fields(ToggleGroup *group, Toggle *toggles[], int count, int selected, t_uint8 active);
+//Initialise the toggle group passed in, needs to be called before all toggle group functions, except for group_set_fields()
 ToggleGroup *group_init(ToggleGroup *group);
-//update a toggle group
+//Updates a toggle group
 int group_update(ToggleGroup *group, float scale_x, float scale_y);
-//render a toggle group
+//Renders a toggle group
 void group_render(ToggleGroup *group);
-//set all toggles states to normal
+//Set the state of all toggles in the toggles list to normal
 void group_clear_selected(ToggleGroup *group);
-//free the toggle group
+//Frees the allocated ressources of the toggle group passed in
 void group_free(ToggleGroup *group);
-//free a list of toggles
+//Frees an array of toggle groups
 void group_list_free(ToggleGroup **groups, int count);
-
+//Updates and renders an array of toggle groups
 int group_list_update_and_render(ToggleGroup *groups[], int count);
 
 #endif
