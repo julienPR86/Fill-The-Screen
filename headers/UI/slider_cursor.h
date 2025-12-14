8313899aa753b43e7a28cddcfe75931a06a4841c
julienPR86 <julien.pr86@gmail.com>
2025-12-14 21:36:40 +0100

    # updated slider cursor header

--- headers/UI/slider_cursor.h
+++ headers/UI/slider_cursor.h
@@ -1,27 +1,27 @@
-#ifndef _SLIDER_CURSOR_H_
-#define _SLIDER_CURSOR_H_
+#ifndef SLIDER_CURSOR_H
+#define SLIDER_CURSOR_H
 
 #include "../color.h"
 #include "UI_Element.h"
 
-//slider cursor structure
+//Structure that represent a slider cursor, a simple rectangle
 typedef struct SliderCursor
 {
-    //The element rect, contains the x, y, width and height values
+    //The cursor rect, contains the position and the dimensions
     UI_Element rect;
-    //cursor state
-    int state;
-    //cursor style
+    //The state of the cursor, see ButtonStates enum
+    t_uint8 state;
+    //The cursor style, contains the background, foreground and hover colors
     SliderCursorStyle *style;
 } SliderCursor;
 
-//Set the fields of the cursor passed in
-SliderCursor *slider_cursor_set_fields(SliderCursor *cursor, int state, SliderCursorStyle *style);
-//slider cursor init, needs to be called before all slider cursor functions
+//Set the fields of the cursor with the values passed in
+SliderCursor *slider_cursor_set_fields(SliderCursor *cursor, t_uint8 state, SliderCursorStyle *style);
+//slider cursor init, needs to be called before all slider cursor functions, except for slider_cursor_set_fields()
 SliderCursor *slider_cursor_init(SliderCursor *cursor);
-//update slider cursor
+//Updates the slider cursor passed in
 int slider_cursor_update(SliderCursor *cursor);
-//render slider cursor
+//Renders the slider cursor passed in
 void slider_cursor_render(SliderCursor *cursor, float scale_x, float scale_y);
 
 #endif
