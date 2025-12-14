36d074b60d6c17c6b10e240589b3b7dcd1ea7623
julienPR86 <julien.pr86@gmail.com>
2025-12-14 22:06:43 +0100

    # updated UI_Element header

--- headers/UI/UI_Element.h
+++ headers/UI/UI_Element.h
@@ -1,51 +1,48 @@
-#ifndef _UI_ELEMENT_H_
-#define _UI_ELEMENT_H_
+#ifndef UI_ELEMENT_H
+#define UI_ELEMENT_H
 
 #include "inline.h"
 #include "outline.h"
 
-//UI_Element struct, it represent the base of an UI element, which contains an x and y position, a width and a height
+//UI_Element struct, it represent the base of an UI object.
+//It contains an x and y position, a width, a height, an outline, an inline, a scale and an anchor
 typedef struct UI_Element
 {
-    //x position
+    //The x position of the element
     float x;
-    //y position
+    //The y position of the element
     float y;
-    //width
+    //The element width
     int width;
-    //height
+    //The element height
     int height;
-    //element outline
+    //The element outline
     Outline outline;
-    //element inline
+    //the elements inline
     Inline inline_;
-    //element local scale
+    //The element local scale
     double scale;
-    //define the element anchor
-    int anchor;
+    //The element anchor
+    t_uint8 anchor;
 } UI_Element;
 
 //Set the fields of element to the values passed in
-UI_Element *UI_Element_set_fields(UI_Element *element, float x, float y, int width, int height, Outline outline, Inline inline_, float scale, int anchor);
-
+UI_Element *UI_Element_set_fields(UI_Element *element, float x, float y, int width, int height, Outline outline, Inline inline_, float scale, t_uint8 anchor);
 //Set the size of element to the size of frame if it's smaller
 //!! Do not put a label rect for element, only for frame if necessary !!
 //!! Set scale_x and scale_y to 1 except if frame is a label !!
 void UI_Element_match_size(UI_Element *element, UI_Element *frame, float scale_x, float scale_y);
-
-//return true if (x, y) is in element, false otherwise
+//Returns true if (x, y) is in element, false otherwise
 int UI_element_collision(UI_Element *element, float x, float y, float scale_x, float scale_y);
 //Positions the element at (x, y) based on the anchor and the scales
 void UI_Element_set_position(UI_Element *element, float x, float y, float x_position_factor, float y_position_factor, float scale_x, float scale_y, int anchor);
-
-//Draw the element inline
+//Draws the element inline
 void UI_Element_render_inline(UI_Element *element, float scale_x, float scale_y);
-//Draw the element outline
+//Draws the element outline
 void UI_Element_render_outline(UI_Element *element, float scale_x, float scale_y);
-
-//return the height of the element
+//Returns the element height
 int UI_Element_get_height(UI_Element *element, float scale_y);
-//return the width of the element
+//Returns the element width
 int UI_Element_get_width(UI_Element *element, float scale_x);
 
 #endif
