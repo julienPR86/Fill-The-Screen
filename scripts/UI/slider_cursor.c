c1ad2d5468997617394ec6d4dc86349ec66621d9
julienPR86 <julien.pr86@gmail.com>
2025-12-16 22:08:59 +0100

    # updated slider cursor rendering

--- scripts/UI/slider_cursor.c
+++ scripts/UI/slider_cursor.c
@@ -32,7 +32,12 @@ void slider_cursor_render(SliderCursor *cursor, float scale_x, float scale_y)
     UI_Element_set_position(&anchored_rect, anchored_rect.x, anchored_rect.y, scale_x, scale_y, scale_x, scale_y, anchored_rect.anchor);
 
     Color color;
-    SDL_FRect cursor_rect = {anchored_rect.x, anchored_rect.y, (anchored_rect.width * scale_x * anchored_rect.scale), (anchored_rect.height * scale_y * anchored_rect.scale)};
+    SDL_FRect cursor_rect = {
+		anchored_rect.x + anchored_rect.inline_.size,
+		anchored_rect.y + anchored_rect.inline_.size,
+		(int)(anchored_rect.width * scale_x * anchored_rect.scale) - (int)(anchored_rect.inline_.size * scale_x * anchored_rect.scale) * 2,
+		(int)(anchored_rect.height * scale_y * anchored_rect.scale) - (int)(anchored_rect.inline_.size * scale_y * anchored_rect.scale) * 2
+	};
 
     switch (cursor->state)
     {
@@ -48,11 +53,11 @@ void slider_cursor_render(SliderCursor *cursor, float scale_x, float scale_y)
         default:
             break;
     }
-    
+
+	UI_Element_render_outline(&anchored_rect, scale_x, scale_y);
+	UI_Element_render_inline(&anchored_rect, scale_x, scale_y);
+
     SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, color.a);
     SDL_RenderFillRect(renderer, &cursor_rect);
-
-    UI_Element_render_outline(&anchored_rect, scale_x, scale_y);
-    UI_Element_render_inline(&anchored_rect, scale_x, scale_y);
     return;
 }
