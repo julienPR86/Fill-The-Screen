d98bc3229bdcdf07b0eaf4153143f40537ec2a47
julienPR86 <julien.pr86@gmail.com>
2025-12-19 23:57:35 +0100

    + added Playerstats structure to store statistics about the current game + added PlayerStats field to Player struct

--- headers/player.h
+++ headers/player.h
@@ -1,6 +1,20 @@
 #ifndef PLAYER_H
 #define PLAYER_H
 
+//Structure that stores statistics about a player
+typedef struct PlayerStats
+{
+	//The number of times the player moved
+	t_uint	moves;
+	//The number of spawned squares considered like a square that fill the map
+	t_uint	filled_squares;
+	//The ratio filled_squares / moves, depends on the active game mode
+	double	filled_square_ratio;
+	//The current map filling percentage
+	double	fill_percent;
+}	PlayerStats;
+
+
 //Structure that represent a player
 typedef struct Player
 {
@@ -12,12 +26,12 @@ typedef struct Player
     t_int8 direction_x;
     //Boolean for the y direction
     t_int8 direction_y;
-    //The number of times the player moved
-    int moves;
     //Tells if the player can move
     t_uint8 can_move;
     //Is set to 1 if the player moved the same frame as the input
     t_uint8 frame_move;
+	//The current player statistics
+	PlayerStats	stats;
 } Player;
 
 //The default player
@@ -35,5 +49,8 @@ void player_set_move_direction(Player *player, t_int8 direction_x, t_int8 direct
 void player_reset(Player *player);
 //Frees allocated ressources of the player passed in
 void	player_destroy(Player *player);
+
+//Updates the player stats
+void	player_stats_update(Player *player, Map *map);
 
 #endif
