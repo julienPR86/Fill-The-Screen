a3c6d30c91fca9c64b8db390a3e6ea20b3ab2997
julienPR86 <julien.pr86@gmail.com>
2026-01-24 17:17:24 +0100

    + added GameState enum and updated header protection name

--- includes/definitions.h
+++ includes/definitions.h
@@ -1,7 +1,20 @@
-#ifndef TYPES_H
-#define TYPES_H
+#ifndef DEFINITIONS_H
+#define DEFINITIONS_H
 
 typedef unsigned int	t_uint;
 typedef unsigned char	t_uint8;
+
+typedef enum e_GameState
+{
+    GAME_STATE_NONE,
+    GAME_STATE_MAIN_MENU,
+    GAME_STATE_OPTIONS,
+    GAME_STATE_MODE_SELECTION,
+    GAME_STATE_GAME,
+	GAME_STATE_NEW_GAME,
+    GAME_STATE_PAUSE_MENU,
+    GAME_STATE_GAME_STATS,
+    GAME_STATE_EXIT_GAME,
+}	t_GameState;
 
 #endif
